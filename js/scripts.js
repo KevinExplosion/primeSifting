51b2107bdc3874f4bb7235fb0e22a8e1391640b0
Kevin Mattison and Matt Rosanio <mmrosani@syr.edu>
2016-01-20 14:18:06 -0800

    remove first test and add logic to pass prime number test

--- js/scripts.js
+++ js/scripts.js
@@ -1,17 +1,20 @@
 function prime_sifter() {
-  var prime = 2;
+  var prime = [2, 3, 5, 7];
   var array = [];
   for(var i = 2; i <= 100; i++) {
     array.push(i);
   }
-  for (var j = 1; j < 99; j++) {
-    if(array[j]%prime === 0) {
-      var firstPart = array.slice(0,j);
-      var secondPart = array.slice((j+1),array.length);
-      console.log(firstPart);
-      console.log(secondPart);
-      array = firstPart.concat(secondPart);
-      console.log(array);
+  for (var k = 0; k < prime.length; k++) {
+    for (var j = 1; j < 99; j++) {
+      if(array[j]%prime[k] === 0) {
+        if(array[j]/prime[k] !== 1) {
+          var firstPart = array.slice(0,j);
+          var secondPart = array.slice((j+1),array.length);
+          console.log(firstPart);
+          array = firstPart.concat(secondPart);
+          console.log(array);
+        }
+      }
     }
   }
   return array
--- spec/specs.js
+++ spec/specs.js
@@ -1,7 +1,4 @@
 describe('prime_sifter', function() {
-  it("will return an array with no even numbers except 2", function() {
-    expect(prime_sifter()).to.eql([2, 3, 5, 7, 9, 11, 13, 15, 17, 19, 21, 23, 25, 27, 29, 31, 33, 35, 37, 39, 41, 43, 45, 47, 49, 51, 53, 55, 57, 59, 61, 63, 65, 67, 69, 71, 73, 75, 77, 79, 81, 83, 85, 87, 89, 91, 93, 95, 97, 99]);
-  });
   it("will return an array of prime numbers", function() {
     expect(prime_sifter()).to.eql([2, 3, 5, 7, 11, 13, 17, 19, 23, 29, 31, 37, 41, 43, 47, 53, 59, 61, 67, 71, 73, 79, 83, 89, 97]);
   });
